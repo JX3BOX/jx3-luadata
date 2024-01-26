f8cc297256b7eb5df8d0179869b339ea35cc6df8
X3ZvaWQ <x3zvawq@gmail.com>
2024-01-26 21:06:27 +0800

    chore: package and type

---
+++ src/types.d.ts
@@ -0,0 +1,13 @@
+declare module "lzfjs" {
+    const compress: (
+        data: ArrayBuffer | TypedArray | Buffer
+    ) => Uint8Array | Buffer;
+    const decompress: (
+        data: ArrayBuffer | TypedArray | Buffer
+    ) => Uint8Array | Buffer;
+
+    export default {
+        compress,
+        decompress,
+    };
+}
